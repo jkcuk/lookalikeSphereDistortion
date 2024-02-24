40734d9fd616320735fe0e69c734a57deb00c336
jkcuk <44874423+jkcuk@users.noreply.github.com>
2024-02-24 08:13:52 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -147,7 +147,7 @@ function gotDevices(deviceInfos) {
  {
   // there is only one camera; assume it's user-facing
   cameraUser = videoStreams[0];
-  cameraEnvironment = null; // leave undefined
+  cameraEnvironment = videoStreams[0]; // TODO for the moment, set it to the user camera
  } else {
   // assume the camera with the higher resolution is the environment-facing camera
   if(videoStreams[0].width*videoStreams[0].height > videoStreams[1].width*videoStreams[1].height)
