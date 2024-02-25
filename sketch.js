18cd9139e87e3928e22110f952e2d16e57c7cc99
jkcuk <44874423+jkcuk@users.noreply.github.com>
2024-02-25 22:23:16 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -76,6 +76,17 @@ function setup() {
   errorText = createP();
   errorText.position(10, 0);
 
+var button = document.createElement("button");
+button.innerHTML = "Do Something";
+
+// 2. Append somewhere
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(button);
+
+// 3. Add event handler
+button.addEventListener ("click", function() {
+  alert("did something");
+});
 
  
  // user-facing camera
