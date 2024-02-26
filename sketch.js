0df0dbcb588377afcf9840f533f2fbae4761c2cf
jkcuk <44874423+jkcuk@users.noreply.github.com>
2024-02-26 13:32:37 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,31 +1,28 @@
 let theShader;
-//let speedtest=1.5;
-//let headingtest=50;
-let PHI1=0;
-let THETA1=180; 
+
+// the cameras
 let cameraUser;
 let cameraEnvironment;
-let videoStreams = [];
-//let VelC;
+
+// components of beta...
 let betax;
 let betay;
 let betaz;
-let phi=0;
-let theta=0;
-// let radius=10;//200;
+// ... and beta and gamma, calculated from them
 let beta;
 let gamma;
-//let textureImg;
-//let motion = false;
-let vxSlider;
-let vySlider;
-let vzSlider;
+
+// direction of beta
+let phi=0;
+let theta=0;
+let PHI1=0;
+let THETA1=180;
+
+let betaxSlider;
+let betaySlider;
+let betazSlider;
 let cameraFOVSlider;
 let screenFOVSlider;
-//let ff;
-//let ff2;
-//let ff3;
-//let ff4;
 let errorText;
 let aspectRatioUser, aspectRatioEnvironment;
 let fudgeFactor;
@@ -41,27 +38,23 @@ function setup() {
  
   createCanvas(windowWidth, windowHeight, WEBGL); 
 
-  // ff = createP();
-  // ff.position(10, 0);
-  // ff.html(`V1.24`);
-
-  vxSlider = createSlider(-1, 1, 0, 0);
-  vxSlider.position(50, 50);
-  vxSlider.size(500);
+  betaxSlider = createSlider(-1, 1, 0, 0);
+  betaxSlider.position(50, 50);
+  betaxSlider.size(500);
   ff2 = createP();
   ff2.position(10, 50-17);
   ff2.html(`&#946;<sub><i>x</i></sub> =`);
  
-  vySlider = createSlider(-1, 1, 0, 0);
-  vySlider.position(50, 100);
-  vySlider.size(500);
+  betaySlider = createSlider(-1, 1, 0, 0);
+  betaySlider.position(50, 100);
+  betaySlider.size(500);
   ff3 = createP();
   ff3.position(10,100-17);
   ff3.html(`&#946;<sub><i>y</i></sub> =`);
  
-  vzSlider = createSlider(-1, 1, 0.25, 0);
-  vzSlider.position(50, 150);
-  vzSlider.size(500);
+  betazSlider = createSlider(-1, 1, 0.25, 0);
+  betazSlider.position(50, 150);
+  betazSlider.size(500);
   ff4 = createP();
   ff4.position(10,150-17);
   ff4.html(`&#946;<sub><i>z</i></sub> =`);
@@ -116,93 +109,31 @@ function setup() {
  };
  cameraEnvironment = createCapture(constraintsEnvironment, done);
  cameraEnvironment.hide();
-
-
- // console.log(cam.getTracks[0].getCapabilities());
-
- /*
-  // Enumerate and create video capture for all available cameras
-  let devices = navigator.mediaDevices.enumerateDevices();
-  devices.then(gotDevices);
- */
       
     camera(0, 0, 0, 0, 0, -1, 0, 1, 0);
     noStroke();
-  //geolocation
- 
   
  perspective(90*PI/180, width/height, 0.01,50000);
-  //perspective(40*PI/180, width/height, 0.001,500);
-  //device orientation
- 
- 
-}
-
-function gotDevices(deviceInfos) {
-  for (let deviceInfo of deviceInfos) {
-    if (deviceInfo.kind === 'videoinput') {
-      let constraints = {
-        video: {
-          deviceId: deviceInfo.deviceId,
-          width: { ideal: 6400 }, // full resolution
-          height: { ideal: 4800 } // full resolution
-        }
-      };
-      let videoStream = createCapture(constraints);
-      videoStream.hide();
-      videoStreams.push(videoStream);
-    }
-  }
-
- // we can deal with a user-facing camera and an environment-facing camera
-
- alert('video streams: '+videoStreams.length);
- 
- // do we have more than one camera?
- if(videoStreams.length == 1)
- {
-  // there is only one camera; assume it's user-facing
-  cameraUser = videoStreams[0];
-  cameraEnvironment = videoStreams[0]; // TODO for the moment, set it to the user camera
- } else {
-  // assume the camera with the higher resolution is the environment-facing camera
-  if(videoStreams[0].width*videoStreams[0].height > videoStreams[1].width*videoStreams[1].height)
-  {
-   // videoStreams[0] is from the environment-facing camera
-   cameraEnvironment = videoStreams[0];
-   cameraUser = videoStreams[1];
-  } else {
-   // videoStreams[1] is from the environment-facing camera
-   cameraEnvironment = videoStreams[1];
-   cameraUser = videoStreams[0];
-  }
- }
-}
-
-function done(ms) {
-  console.log('MediaStream: '+ms);
-  console.log('tracks[0]: '+ms.getVideoTracks()[0]);
- console.log('capabilities: '+ms.getVideoTracks()[0].getCapabilities());
-
- console.log('facing mode: '+ms.getVideoTracks()[0].getCapabilities().facingMode);
-//             facing mode: ${cameraEnvironment.getUserMedia().getTracks[0].getCapabilities().facingMode}
 }
 
 function draw() {
    background(0);
 
-  //background(200); 
  angleMode(DEGREES);
-  betax=vxSlider.value();
-  betay=vySlider.value();
-  betaz=vzSlider.value();
+  betax=betaxSlider.value();
+  betay=betaySlider.value();
+  betaz=betazSlider.value();
  let beta2=betax*betax+betay*betay+betaz*betaz;
 
   if (beta2 >=1 ){
-  const contentString = "Beta >= 1, setting it to 0.99";
-  errorText.html(contentString.fontcolor("red")); 
-  
-  beta = 0.99; 
+   const contentString = "Beta >= 1, setting it to 0.99";
+   errorText.html(contentString.fontcolor("red")); 
+
+   beta0 = sqrt(beta2);
+   beta = 0.99; 
+   betax *= beta/beta0;
+   betay *= beta/beta0;
+   betaz *= beta/beta0;
  } else{
   beta=sqrt(beta2);
  }
