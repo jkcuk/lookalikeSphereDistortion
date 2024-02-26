f0e9e44676ce1b8cec983b0a75050770d14ea973
jkcuk <44874423+jkcuk@users.noreply.github.com>
2024-02-26 14:01:09 +0000

    Improved display behaviour

--- sketch.js
+++ sketch.js
@@ -149,7 +149,7 @@ function draw() {
 
   if(cameraUser) aspectRatioUser=cameraUser.width/cameraUser.height;
   if(cameraEnvironment) aspectRatioEnvironment=cameraEnvironment.width/cameraEnvironment.height;
-  errorText.html(`environment camera: ${cameraEnvironment.width}x${cameraEnvironment.height}, user camera: ${cameraUser.width}x${cameraUser.height}, beta: (${betax}, ${betay}, ${betaz})`);
+  errorText.html(`environment camera: ${cameraEnvironment.width}x${cameraEnvironment.height}, user camera: ${cameraUser.width}x${cameraUser.height}, beta: (${betax.toFixed(2)}, ${betay.toFixed(2)}, ${betaz.toFixed(2)})`);
 
   // distorted lookalike sphere
   fudgeFactor = Math.tan(0.5*Math.PI/180.0*cameraFOVSlider.value());
